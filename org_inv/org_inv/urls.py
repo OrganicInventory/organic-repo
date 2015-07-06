7becb969dab1680c2003da9c51ca10766cb46e27
Hannah <hdeters07@gmail.com>
2015-07-06 13:53:04 -0400

    products view

--- org_inv/org_inv/urls.py
+++ org_inv/org_inv/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import inventory.views as inv_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^products/$', inv_views.AllProductsView.as_view(), name="all_products"),
 ]
