78bf4b97ed1f4c917e6667f552a758a2ef680865
Hannah <hdeters07@gmail.com>
2015-07-15 09:43:53 -0400

    views new line

--- org_inv/inventory/views.py
+++ org_inv/inventory/views.py
@@ -4,6 +4,7 @@ import math
 from django.contrib import messages
 from django.core.mail import send_mail
 from org_inv import settings
+
 import re
 from factual import Factual
 from django.contrib.auth.decorators import login_required
