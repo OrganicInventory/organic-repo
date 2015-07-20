d4f4067d2b1ea5e71042229357db37d3d8681e9a
jashelton <justin.shelton@yahoo.com>
2015-07-20 18:00:07 -0400

    prep to pull

--- org_inv/inventory/views.py
+++ org_inv/inventory/views.py
@@ -917,7 +917,7 @@ def get_all_service_data(request):
 #######################################################################################################################
 
 def get_product(upc_code):
-    factual = Factual("NKesunTqQ4HJkZuf0snbTjvn1F6gKMG8DwTPJJVh", "suzMzZZLymEzvXHPm5BBO8dg8Zgy1FSeHBfX6Xae")
+    factual = Factual("gCKclwfy6eBki5UyHDxS56x7zmcvCMaGJ7l7v9cM", "Dt8V4ngb484Blmyaw5G9SxbycgpOsJL0ENckwxX0")
     products = factual.table('products')
     data = products.filters({'upc': {'$includes': upc_code}}).data()
     if data:
